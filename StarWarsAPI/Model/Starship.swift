900d784357a5556af9f258561760001a44c15989
Zebadiah <zebadiahww@gmail.com>
2019-10-03 17:58:58 -0600

    model built

---
+++ StarWarsAPI/Model/Starship.swift
@@ -0,0 +1,32 @@
+//
+//  Starship.swift
+//  StarWarsAPI
+//
+//  Created by Zebadiah Watson on 10/3/19.
+//  Copyright © 2019 Zebadiah Watson. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelDectionary: Codable {
+    let starships: [Starship]
+}
+
+struct Starship: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case model = "model"
+        case name = "name"
+        case cost = "cost_in_credits"
+        case speed = "max_atmosphering_speed"
+        case films = "films"
+    }
+    
+    let name: String
+    let model: String
+    let cost: String
+    let speed: String
+    let films: [String]
+    
+}
+
